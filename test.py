0c373c51c7a7772b4950cb20d2b141412f67b4c5
vorphan <vorphan@qq.com>
2017-10-11 21:33:29 +0800

    增加测试数据3

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ from selenium.common.exceptions import TimeoutException
 # taopiaopiaoSpider(
 #             "https://dianying.taobao.com/showDetail.htm?spm=a1z21.3046609.w2.4.IRhyJR&showId=199280&n_s=new&source=current")
 
+num_success,num__nosuch,num_stale,num_time,num_others=0,0,0,0,0
+
 for i in range(1, 101):
     try:
         start_time=time.localtime()
@@ -16,30 +18,35 @@ for i in range(1, 101):
             "https://dianying.taobao.com/showDetail.htm?spm=a1z21.3046609.w2.4.IRhyJR&showId=199280&n_s=new&source=current")
         f = open("out.txt", "a")  # 打开文件
         print("第%d次测试成功" % i, file=f)
+        num_success+=1
         f.close()
     except NoSuchElementException as e1:
         f = open("out.txt", "a")  # 打开文件
         print("第%d次测试发生错误，错误原因： NoSuchElementException" % i, file=f)
         print(e1, file=f)
         traceback.print_exc(file=f)
+        num__nosuch+=1
         f.close()
     except StaleElementReferenceException as e2:
         f = open("out.txt", "a")  # 打开文件
         print("第%d次测试发生错误，错误原因： StaleElementReferenceException" % i, file=f)
         print(e2, file=f)
         traceback.print_exc(file=f)
+        num_stale+=1
         f.close()
     except TimeoutException as e3:
         f = open("out.txt", "a")  # 打开文件
         print("第%d次测试发生错误，错误原因： TimeoutException" % i, file=f)
         print(e3, file=f)
         traceback.print_exc(file=f)
+        num_time+=1
         f.close()
     except Exception as e4:
         f = open("out.txt", "a")  # 打开文件
         print("第%d次测试发生错误，错误原因： 未知错误类型" % i, file=f)
         print(e4, file=f)
         traceback.print_exc(file=f)
+        num_others+=1
         f.close()
     finally:
         end_time=time.localtime()
@@ -47,4 +54,10 @@ for i in range(1, 101):
         f = open("out.txt", "a")  # 打开文件
         print('第%d次测试于%s结束\n' %(i,time.strftime("%Y-%m-%d %H:%M:%S",end_time)),file=f)
         f.close()
-        #相隔时间，程序100次后输出统计报告
+f = open("out.txt", "a")  # 打开文件
+print('统计：共有%d次成功，\n\
+        %d次 NoSuchElementException 错误，\n\
+        %d次 StaleElementReferenceException 错误，\n\
+        %d次 TimeoutException 错误，\n\
+        %d次 其他 Exception 错误' %(num_success,num__nosuch,num_stale,num_time,num_others),file=f)
+f.close()
